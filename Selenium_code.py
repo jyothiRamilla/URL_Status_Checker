6dca11396802a1db4d553c18a87250d18aa8216d
Jyothi Ramilla <50903720+jyothiRamilla@users.noreply.github.com>
2021-10-23 16:01:50 +0530

    clean the code

--- Selenium_code.py
+++ Selenium_code.py
@@ -1,13 +1,11 @@
 import os
 import requests
 from selenium import webdriver
-#from webdriver_manager.chrome import ChromeDriverManager
 from db import connect_mongo
  
     
     
 def depth_scraping(url_link,depth,driver,urllink_collection={}):
-    #if(depth==1):    
     try:
         driver.get(url_link)
         links = driver.find_elements_by_css_selector("link")
@@ -18,7 +16,6 @@ def depth_scraping(url_link,depth,driver,urllink_collection={}):
         for val in range(1,depth):
             for link in urllink_collection[val]:
                 try:
-                    #driver = get_driver_link(link)
                     driver.get(url_link)
                     urllink_collection[val+1].append(driver.find_elements_by_css_selector("link"))
                 except Exception:
@@ -33,20 +30,6 @@ def depth_scraping(url_link,depth,driver,urllink_collection={}):
 def Url_link_checker(url_link,depth=1,url_dict={}):
     # Access collection of the database     
     collection= connect_mongo.connect_db()
-    #options = webdriver.ChromeOptions() 
-    #options.add_argument("start-maximized")
-    #options.add_argument('disable-infobars')
-    """
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    #links = depth_scraping(url_link=url_link,depth=depth)
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    print(driver)
-    #driver = webdriver.Chrome(ChromeDriverManager().install())
-    """
     CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
     chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
     options = webdriver.ChromeOptions()
@@ -57,25 +40,7 @@ def Url_link_checker(url_link,depth=1,url_dict={}):
     options.add_argument('window-size=1200x600')
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
     links = depth_scraping(url_link,depth,driver,urllink_collection={})
-    #links = driver.find_elements_by_css_selector("link")
-    print("Links are")
-    print(links)
-    
-    """
-    for link in links: 
-        if(link!=None):
-            try:                    
-                r = requests.head(link.get_attribute('href'))
-                ##Insert each record at a time to the database
-                collection.insert_one({"Domain_Name":url_link,"URL":link.get_attribute('href'),"status_code":r.status_code,"Response_time":r.elapsed.total_seconds()})
-
-                url_dict[link.get_attribute('href')] = [{"Domain_Name":url_link,"URL":link.get_attribute('href'),"status_code":r.status_code,"Response_time":r.elapsed.total_seconds()}]
-
-            except Exception:
-                pass            
-
-    return url_dict
-    """
+   
     for key,value in links.items():
         for link in value:
             if(link!=None):
